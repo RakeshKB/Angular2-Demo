783e831c5144399359d85d8755a75112caa80909
rakesh.kb <rakesh.kb@gmail.com>
2016-10-05 10:20:25 +0530

    Comments

---
+++ app/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+	selector: 'my-app'
+})
+
+@View({
+	template: '<h2>I started learning Angular2</h2><p>hello</p>'
+})
+
+export class AppComponent{
+	
+}
